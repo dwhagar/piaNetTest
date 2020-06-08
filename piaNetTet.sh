608fcf3df63cbdef8ffb2e5a577890a793c141dc
David Hagar <david.hagar@gmail.com>
2020-06-08 01:17:00 -0400

    Fixed Bugs, Added Non-WiFi Support
    Fixed some bugs with the string parsing.
    Added support for Non-WiFi Network Connections
    Switched to awk instead of cut to parse WiFi Name

--- piaNetTet.sh
+++ piaNetTet.sh
@@ -1,23 +1,17 @@
 #/bin/bash
-set -eo pipefail
 # Make changes to VPN connection based on network status.
-
-# TODO:  This script currently only cares about WiFi, should put effort into
-#        making it work for a wired network as well, just not sure how.
-
-# As a side-nite this script isn't really designed for having no trusted
-# networks and thus this could introduce a bug, it has not been tested.
 
 # How do we tell we are online?
 PINGTARGET=8.8.8.8
-PINGCMD="/sbin/ping -c 1 $PINGTARGET"
-INTERFACE=en0
+PINGCMD=/sbin/ping
+WIFI=en0
 
 # How do we control PIA?
 PIACMD=/usr/local/bin/piactl
 PIAREGION=auto
 
 # How do we tell what we're connected to?
+NETINTCMD=/sbin/route
 AIRPORTCMD=/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport
 
 # Read last network state.
@@ -44,37 +38,72 @@ else
 fi
 
 # Read the current network state.
-CURRENTNET=$($AIRPORTCMD $INTERFACE -I|grep -v BSSID|grep SSID|xargs|cut -c7-)
+CURRENTINT=$($NETINTCMD get default 2>&1|grep interface|awk '{print $2}')
 
+# If the current network interface is the WiFi interface, proceed with WiFi
+# information.  Set a flag so the rest of the script knows what to do.
+if [[ $CURRENTINT == $WIFI ]]; then
+	# If WiFi is in use, use the network SSID for the current network.
+	USINGWIFI=1
+	TESTNET=1
+	CURRENTNET=$($AIRPORTCMD $WIFI -I|grep -v BSSID|grep SSID|xargs|awk '{print $2}')
+elif [[ $CURRENTINT != "" ]]; then
+	# If WiFi is not in use but there is an active network connect use the
+	# interface for the current network.
+	USINGWIFI=0
+	TESTNET=1
+	CURRENTNET=$CURRENTINT
+else
+	# If there is no active network interface, assume the system is offline.
+	TESTNET=0
+fi
+	
+# Check to see if anything has changed.
 if [[ $LASTNET == $CURRENTNET ]]; then
-	# Don't do anything if a chance in the network has not occurred.
+	# Don't do anything if a change in the network has not occurred.
 	exit 0
 else
-	# Only check on PIA status if a change in the network has occurred.
-	PIASTATUS=$($PIACMD get connectionstate)
-	
 	# Output information for the log.
 	echo $(/bin/date)
 	echo "Network state has changed since last check."
-	echo "Pinging $PINGTARGET to test Internet connectivity..."
 	
-	$PINGCMD &> /dev/null
-
-	if (( $? > 0)); then
+	
+	if (( $TESTNET > 0 )); then
+		# If we're not sure about the network, better ping the Internet.
+		echo "Pinging $PINGTARGET to test Internet connectivity..."
+	
+		$PINGCMD -c 1  $PINGTARGET &> /dev/null
+	
+		# Is there or isn't there Internet.
+		if (( $? > 0 )); then
+			echo "The network is online but the Internet is not."
+			NONET=1
+		else
+			NONET=0
+		fi
+	else
+		# If we already know the network is dead, the Internet will be too.
+		echo "The network is offline."
+		NONET=1
+	fi
+	
+	# Find out what PIA is doing.
+	PIASTATUS=$($PIACMD get connectionstate)
+	
+	# What to do if the Internet is not online.
+	if (( $NONET > 0 )); then
 		# Network is offline.
-		echo "The Internet is offline."
-
 		# If PIA is connected, disconnect it.
-		if [[ $PIASTATUS == "Connected" ]]; then
+		if [[ $PIASTATUS == "Connected" || $PIASTATUS == "Connecting" ]]; then
 			echo "Disconnecting from PIA."
 			$PIACMD disconnect
 		fi
 	else
 		# Network is online.
 		ONTRUSTEDNET=0 # Stet this flag, assume no trusted networks.
-	
+
 		echo "The Internet is online and connected to $CURRENTNET."
-		
+	
 		# If there are no trusted networks, this should not be done.
 		if [[ $TRUSTEDNETS != "" ]]; then
 			# Find out if we're on a trusted net.
@@ -89,7 +118,7 @@ else
 		
 		# Now decide what to do with PIA.
 		if (( $ONTRUSTEDNET > 0 )); then
-			if [[ $PIASTATUS == "Connected" ]]; then
+			if [[ $PIASTATUS == "Connected" || $PIASTATUS == "Connecting" ]]; then
 				echo "Current network is trusted, disconnecting from PIA."
 				$PIACMD disconnect
 			fi
